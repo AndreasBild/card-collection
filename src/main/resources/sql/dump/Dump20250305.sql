948e7f4a6ea328c3be107ef245ee54c27eef5b73
abild <andreas.bild@gmail.com>
2025-05-27 14:02:27 +0200

    added SQL dump

---
+++ src/main/resources/sql/dump/Dump20250305.sql
@@ -0,0 +1,224 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for macos14 (arm64)
+--
+-- Host: localhost    Database: cardcollection
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Card`
+--
+
+DROP TABLE IF EXISTS `Card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Card` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `print_run` int NOT NULL,
+  `serial_number` int NOT NULL,
+  `season` varchar(10) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `rookie_card` tinyint(1) NOT NULL DEFAULT '0',
+  `game_used_material` tinyint(1) NOT NULL DEFAULT '0',
+  `player_id` bigint DEFAULT NULL,
+  `theme_id` bigint DEFAULT NULL,
+  `autograph` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_card_player` (`player_id`),
+  KEY `fk_card_theme` (`theme_id`),
+  CONSTRAINT `fk_card_player` FOREIGN KEY (`player_id`) REFERENCES `Player` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_card_theme` FOREIGN KEY (`theme_id`) REFERENCES `card_theme` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Card`
+--
+
+LOCK TABLES `Card` WRITE;
+/*!40000 ALTER TABLE `Card` DISABLE KEYS */;
+INSERT INTO `Card` VALUES (1,1000,10000,'1994-95','378',1,0,1,1,0);
+/*!40000 ALTER TABLE `Card` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Card_Brand`
+--
+
+DROP TABLE IF EXISTS `Card_Brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Card_Brand` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `manufacturer_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cardbrand_manufacturer` (`manufacturer_id`),
+  CONSTRAINT `fk_cardbrand_manufacturer` FOREIGN KEY (`manufacturer_id`) REFERENCES `card_manufacturer` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Card_Brand`
+--
+
+LOCK TABLES `Card_Brand` WRITE;
+/*!40000 ALTER TABLE `Card_Brand` DISABLE KEYS */;
+INSERT INTO `Card_Brand` VALUES (1,'Collectors Choice',1),(2,'Exquisite',1),(3,'Authentic',1);
+/*!40000 ALTER TABLE `Card_Brand` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `card_manufacturer`
+--
+
+DROP TABLE IF EXISTS `card_manufacturer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `card_manufacturer` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card_manufacturer`
+--
+
+LOCK TABLES `card_manufacturer` WRITE;
+/*!40000 ALTER TABLE `card_manufacturer` DISABLE KEYS */;
+INSERT INTO `card_manufacturer` VALUES (1,'Upper Deck'),(2,'Topps'),(3,'Fleer'),(4,'Leaf'),(5,'Panini');
+/*!40000 ALTER TABLE `card_manufacturer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `card_theme`
+--
+
+DROP TABLE IF EXISTS `card_theme`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `card_theme` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `brand_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cardtheme_brand` (`brand_id`),
+  CONSTRAINT `fk_cardtheme_brand` FOREIGN KEY (`brand_id`) REFERENCES `card_brand` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card_theme`
+--
+
+LOCK TABLES `card_theme` WRITE;
+/*!40000 ALTER TABLE `card_theme` DISABLE KEYS */;
+INSERT INTO `card_theme` VALUES (1,'You Crash The Game',1);
+/*!40000 ALTER TABLE `card_theme` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Player`
+--
+
+DROP TABLE IF EXISTS `Player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Player` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `team_id` bigint DEFAULT NULL,
+  `sport_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_player_team` (`team_id`),
+  KEY `fk_player_sport_idx` (`sport_id`),
+  CONSTRAINT `fk_player_sport` FOREIGN KEY (`sport_id`) REFERENCES `Sport` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT,
+  CONSTRAINT `fk_player_team` FOREIGN KEY (`team_id`) REFERENCES `Team` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Player`
+--
+
+LOCK TABLES `Player` WRITE;
+/*!40000 ALTER TABLE `Player` DISABLE KEYS */;
+INSERT INTO `Player` VALUES (1,'Juwan','Howard',1,1);
+/*!40000 ALTER TABLE `Player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Sport`
+--
+
+DROP TABLE IF EXISTS `Sport`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Sport` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Sport`
+--
+
+LOCK TABLES `Sport` WRITE;
+/*!40000 ALTER TABLE `Sport` DISABLE KEYS */;
+INSERT INTO `Sport` VALUES (1,'Basketball');
+/*!40000 ALTER TABLE `Sport` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Team`
+--
+
+DROP TABLE IF EXISTS `Team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Team` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Team`
+--
+
+LOCK TABLES `Team` WRITE;
+/*!40000 ALTER TABLE `Team` DISABLE KEYS */;
+INSERT INTO `Team` VALUES (1,'Washington Wizard'),(2,'Denver Nuggets'),(3,'Dallas Mavericks');
+/*!40000 ALTER TABLE `Team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cardcollection'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-05  9:42:42
