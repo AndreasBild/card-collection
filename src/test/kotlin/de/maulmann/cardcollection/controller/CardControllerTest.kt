10e7cf89b560a58099a33d271f613505a16ffbac
abild <andreas.bild@gmail.com>
2025-07-07 10:41:24 +0200

    added new mockito version

--- src/test/kotlin/de/maulmann/cardcollection/controller/CardControllerTest.kt
+++ src/test/kotlin/de/maulmann/cardcollection/controller/CardControllerTest.kt
@@ -8,13 +8,12 @@ import org.junit.jupiter.api.Test
 import org.mockito.kotlin.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get // Using Kotlin DSL for MockMvc
+import org.springframework.test.web.servlet.get
 
 @WebMvcTest(CardController::class)
 class CardControllerTest {
@@ -22,13 +21,13 @@ class CardControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     private lateinit var cardService: CardService
 
-    @MockBean
+    @MockitoBean
     private lateinit var cardManufacturerService: CardManufacturerService
 
-    @MockBean
+    @MockitoBean
     private lateinit var playerService: PlayerService
 
     @Test
--- src/test/kotlin/de/maulmann/cardcollection/controller/FilterDataControllerTest.kt
+++ src/test/kotlin/de/maulmann/cardcollection/controller/FilterDataControllerTest.kt
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test
 import org.mockito.kotlin.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get // Using Kotlin DSL for MockMvc
 
@@ -20,7 +20,7 @@ class FilterDataControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     private lateinit var cardService: CardService
 
     @Autowired
