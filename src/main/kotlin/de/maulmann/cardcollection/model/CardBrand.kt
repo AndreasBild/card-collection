0c33984f663d697d3446895a5b153aa706824df8
andreasbild <andreas.bild@gmail.com>
2026-05-22 23:32:02 +0200

    added cacheable annotations

--- src/main/kotlin/de/maulmann/cardcollection/model/CardBrand.kt
+++ src/main/kotlin/de/maulmann/cardcollection/model/CardBrand.kt
@@ -1,14 +1,11 @@
 package de.maulmann.cardcollection.model
 
-
-import jakarta.persistence.*
 import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
 
 @Entity
 class CardBrand(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0, val name: String,
-
-    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "manufacturer_id") @JsonIgnore
-
-    val manufacturer: CardManufacturer
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0,
+    val name: String,
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "manufacturer_id") @JsonIgnore val manufacturer: CardManufacturer
 )
