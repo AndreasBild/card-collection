cf272217377ea465a7f81e333b38088f43a249b5
andreasbild <andreas.bild@gmail.com>
2026-07-12 00:37:50 +0200

    added caches for performance optimization

--- src/main/resources/db/migration/V6__Add_indexes.sql
+++ src/main/resources/db/migration/V6__Add_indexes.sql
@@ -10,3 +10,4 @@ CREATE INDEX idx_player_surname_name ON player(surname, name);
 
 CREATE INDEX idx_card_theme_brand_id ON card_theme(brand_id);
 CREATE INDEX idx_card_brand_manufacturer_id ON card_brand(manufacturer_id);
+CREATE INDEX idx_card_print_run ON card(print_run);
